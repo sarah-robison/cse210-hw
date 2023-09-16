db11c7e8b364d3c97128ee1bc3ed41697b78d86e
Sarah Robison <s.e.hansen027@gmail.com>
2023-09-15 23:33:34 -0600

    Just the bare bones of the assignment. Haven't yet included the + and - stuff.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string letter;
+
+        if (number >= 90)
+        {
+            //Console.WriteLine("You have an A in the class.");
+            letter = "A";
+        }
+
+        else if (number >= 80)
+        {
+            //Console.WriteLine("You have a B in the class.");
+            letter = "B";
+        }
+
+        else if (number >= 70)
+        {
+            //Console.WriteLine("You have a C in the class.");
+            letter = "C";
+        }
+
+        else if (number >= 60)
+        {
+            //Console.WriteLine("You have a D in the class.");
+            letter = "D";
+        }
+
+        else
+        {
+            //Console.WriteLine("You have an F in the class.");
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got the letter grade {letter}.");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+
+        else
+        {
+            Console.WriteLine("You do not have a passing grade. Keep working hard for next time!");
+        }
+    
     }
 }
