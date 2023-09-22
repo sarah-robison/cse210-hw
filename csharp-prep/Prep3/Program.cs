566358f1e9cd119e8587c88bd5fdd24248c1716a
Sarah Robison <s.e.hansen027@gmail.com>
2023-09-21 23:31:53 -0600

    It works! Didn't have time for stretch challenges.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guess_number = int.Parse(guess);
+
+        while (guess_number != number)
+        {
+
+        if (guess_number > number)
+            Console.WriteLine("Lower");
+        
+        else if (guess_number < number)
+            Console.WriteLine("Higher");
+        
+        Console.Write("What is you new guess? ");
+        guess = Console.ReadLine();
+        guess_number = int.Parse(guess);
+
+        }
+
+        Console.WriteLine("You guessed correct!");
     }
 }
